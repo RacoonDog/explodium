26bc4ddbfff917de86fd0bbcd7f1040a41a8edfe
RacoonDog <32882447+racoondog@users.noreply.github.com>
2025-02-24 00:16:32 -0500

    setup

---
+++ src/main/java/io/github/racoondog/explodium/Explodium.java
@@ -0,0 +1,14 @@
+package io.github.racoondog.explodium;
+
+import com.mojang.logging.LogUtils;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+
+public class Explodium implements ModInitializer {
+	public static final Logger LOGGER = LogUtils.getLogger();
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/io/github/racoondog/explodium/mixin/ExplosionImplMixin.java
@@ -0,0 +1,8 @@
+package io.github.racoondog.explodium.mixin;
+
+import net.minecraft.world.explosion.ExplosionImpl;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ExplosionImpl.class)
+public class ExplosionImplMixin {
+}
